3f30a876add30893f21ad7132d9bdaf489c0d8a3
llne <532416583@qq.com>
2023-03-30 15:16:10 +0800

    lab2_3 done

--- kernel/strap.c
+++ kernel/strap.c
@@ -60,7 +60,7 @@ void handle_user_page_fault(uint64 mcause, uint64 sepc, uint64 stval) {
       // hint: first allocate a new physical page, and then, maps the new page to the
       // virtual address that causes the page fault.
       //panic( "You need to implement the operations that actually handle the page fault in lab2_3.\n" );
-  user_vm_map((pagetable_t)current->pagetable, stval/(PGSIZE)*(PGSIZE), PGSIZE,(uint64)alloc_page(), prot_to_type(PROT_WRITE | PROT_READ, 1));
+      user_vm_map((pagetable_t)current->pagetable, stval/(PGSIZE)*(PGSIZE), PGSIZE,(uint64)alloc_page(), prot_to_type(PROT_WRITE | PROT_READ, 1));
       break;
     default:
       sprint("unknown page fault.\n");
