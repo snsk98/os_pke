f785c80734db822439f867ed1063a55592a51a22
llne <532416583@qq.com>
2023-03-30 15:10:47 +0800

    lab2_2 done

---
+++ obj/kernel/kernel.d
@@ -0,0 +1,17 @@
+obj/kernel/kernel.o: kernel/kernel.c kernel/riscv.h util/types.h \
+ kernel/config.h kernel/elf.h kernel/process.h kernel/pmm.h kernel/vmm.h \
+ kernel/memlayout.h spike_interface/spike_utils.h \
+ spike_interface/spike_file.h spike_interface/spike_memory.h \
+ spike_interface/spike_htif.h
+kernel/riscv.h:
+util/types.h:
+kernel/config.h:
+kernel/elf.h:
+kernel/process.h:
+kernel/pmm.h:
+kernel/vmm.h:
+kernel/memlayout.h:
+spike_interface/spike_utils.h:
+spike_interface/spike_file.h:
+spike_interface/spike_memory.h:
+spike_interface/spike_htif.h:
---
+++ obj/user/app_naive_malloc.d
@@ -0,0 +1,4 @@
+obj/user/app_naive_malloc.o: user/app_naive_malloc.c user/user_lib.h \
+ util/types.h
+user/user_lib.h:
+util/types.h:
